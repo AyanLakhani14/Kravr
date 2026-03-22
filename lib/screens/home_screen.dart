40c62d609fb97f28a587ba57016b21f3acd24153
AyanLakhani14 <ayanlakhani21@gmail.com>
2026-03-22 13:03:35 -0400

    Connect home screen to SQLite and display food spots list

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
+import '../database/db_helper.dart';
+import '../models/food_spot.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<FoodSpot> spots = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    final data = await DBHelper.instance.getAllFoodSpots();
+    setState(() {
+      spots = data;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Kravr')),
-      body: const Center(
-        child: Text('Home Screen'),
-      ),
+      body: spots.isEmpty
+          ? const Center(child: Text('No food spots yet'))
+          : ListView.builder(
+              itemCount: spots.length,
+              itemBuilder: (context, index) {
+                final spot = spots[index];
+                return ListTile(
+                  title: Text(spot.name),
+                  subtitle: Text(spot.cuisine),
+                );
+              },
+            ),
     );
   }
 }
